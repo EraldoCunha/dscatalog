d70690adf42fa084ae1121b888e813f55ab77dbc
Eraldo Cunha <eraldo_cunha@hotmail.com>
2025-07-14 11:01:43 -0300

    Sign up case

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserInsertDTO.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dscatalog.dto;
 
 import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.io.Serial;
 
@@ -9,6 +11,8 @@ public class UserInsertDTO extends UserDTO {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @NotBlank(message = "Campo obrigatório")
+    @Size(min = 8, message = "Deve ter no mínimo 8 caracteres")
     private String password;
 
     public UserInsertDTO(){
